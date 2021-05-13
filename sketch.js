4fa4e5af76f7b6b8fcea02dd8a2c66be32a65e6a
Stephanie Grasso <sngrasso@dons.usfca.edu>
2021-05-13 12:14:50 -0700

    finished

--- sketch.js
+++ sketch.js
@@ -41,6 +41,7 @@ const cl_CityPays_CutParks = 11;
 // anger emojis
 var angerImage;   // anger emoji
 var maxAnger = 5;
+var allEndings = [];
 
 // character arrays
 var characterImages = [];   // array of character images, keep global for future expansion
@@ -56,6 +57,7 @@ const moms = 5;
 
 // my variables
 let budget = 50000;
+let helpedEdu = 0;
 
 
 // room indices - look at adventureManager
@@ -76,9 +78,11 @@ const salaryScreen = 16;
 const productScreen = 17;
 const rumorScreen = 18;
 const ignoreScreen = 19;
+const sellAltScreen = 20;
 
 let headlineFont;
 let bodyFont;
+let endFont;
 
 
 // Allocate Adventure Manager with states table and interaction tables
@@ -87,7 +91,7 @@ function preload() {
   // headlineFont = loadFont('fonts/AstroSpace-0Wl3o.otf');
   headlineFont = loadFont('fonts/continuum/contm.ttf');
   bodyFont = loadFont('fonts/Ubuntu-Regular.ttf');
-  // bodyFont = loadFont('fonts/continuum/contm.ttf');
+  endFont = loadFont('fonts/continuum/contb.ttf');
 
   // load all images
   angerImage = loadImage("assets/anger_emoji.png");
@@ -117,7 +121,9 @@ function setup() {
 
   // call OUR function to setup additional information about the p5.clickables
   // that are not in the array 
-  setupClickables(); 
+  setupClickables();
+
+  setupEndings();
 
   fs = fullscreen();
   console.log("finished setup")
@@ -173,8 +179,8 @@ function drawCharacters() {
 function setupClickables() {
   // All clickables to have same effects
   for( let i = 0; i < clickables.length; i++ ) {
-    clickables[i].width = 500;
-    clickables[i].height = 75;
+    clickables[i].width = 1000;
+    clickables[i].height = 60;
     clickables[i].textSize = 20;
     clickables[i].strokeWeight = 0;
     clickables[i].cornerRadius = 30;
@@ -191,18 +197,29 @@ function setupClickables() {
 
   // we do specific callbacks for each clickable
   clickables[6].onPress = clAddSecurity;
+  clickables[3].onPress = clRestart;
+  clickables[10].onPress = clAddFeatureUD;
+  clickables[12].onPress = clAddFeatureLD;
+  clickables[14].onPress = clAddFeatureLD2;
+  clickables[16].onPress = clEndEdu;
+  clickables[24].onPress = clEndEdu;
 
 
 }
 
 // tint when mouse is over
 clickableButtonHover = function () {
-  this.color = "#00b3d6";
+  if (this.name === "Start_Over"){
+    this.color = "#904eed"
+    this.stroke= "#7122ba"
+  } else {
+    this.color = "#00b3d6";
+    this.stroke = "#007c9e"
+  }
   this.noTint = false;
   this.tint = "#FF0000";
   this.textColor = "#FFFFFF";
   this.strokeWeight = 2;
-  this.stroke = "#007c9e"
 }
 
 // color a light gray if off
@@ -227,56 +244,47 @@ clEnding = function() {
   adventureManager.clickablePressed(this.name);
 }
 
+clRestart = function() {
+  console.log("restarting")
+  budget = 50000
+  helpedEdu = 0;
+  adventureManager.clickablePressed(this.name);
+}
+
 clAddSecurity = function() {
   console.log("security")
   budget -= 25000;
   adventureManager.clickablePressed(this.name);
 }
 
-clBuildRival = function() {
-  characters[upperStudents].addAnger(2);
-  characters[moms].subAnger(1);
-  characters[shareHold].addAnger(1);
-  characters[developers].addAnger(1);
+clAddFeatureUD = function() {
+  console.log("security")
+  budget -= 35000;
+  helpedEdu += 1;
   adventureManager.clickablePressed(this.name);
 }
 
-clIgnoreThem = function() {
-  characters[upperStudents].addAnger(1);
-  characters[lowerStudents].addAnger(1);
-  characters[developers].addAnger(1);
+clAddFeatureLD = function() {
+  console.log("security")
+  budget -= 30000;
+  helpedEdu += 1;
   adventureManager.clickablePressed(this.name);
 }
 
-clCutArts = function() {
-  characters[lowerStudents].addAnger(2);
-  characters[moms].addAnger(2);
-  characters[govern].addAnger(1);
+clAddFeatureLD2 = function() {
+  console.log("security")
+  budget -= 10000;
+  helpedEdu += 1;
   adventureManager.clickablePressed(this.name);
 }
 
-clCutTransportation = function() {
-  characters[lowerStudents].addAnger(3);
-  characters[govern].addAnger(1);
-  characters[moms].addAnger(1);
-  adventureManager.clickablePressed(this.name);
+clEndEdu = function () {
+  if(helpedEdu > 0) {
+    adventureManager.clickablePressed("Sell_Edu");
+  } else {
+    adventureManager.clickablePressed("Sell");
+  }
 }
-
-clCutCityWages = function() {
-  characters[govern].addAnger(2);
-  characters[developers].addAnger(2);
-  characters[moms].addAnger(1);
-  adventureManager.clickablePressed(this.name);
-}
-
-clCutParks = function() {
-  characters[govern].addAnger(1);
-  characters[lowerStudents].addAnger(2);
-  characters[moms].addAnger(1);
-  adventureManager.clickablePressed(this.name);
-}
-
-
 
 
 
@@ -373,16 +381,16 @@ function loadAllText() {
   scenarioRooms[eduScreen].setText("Pick an Age Group","Which age group would you like to invest into?");
   scenarioRooms[eduUpperScreen].setText("Extended Features","Device is being mainly used and tested within a group dynamic. More students feel more encouraged to participate in group projects and feel more productive. However; they express how they would like for more features geared towards group related tasks to be included. For example, a way to download previous thought projections and save them to a local computer.");
   scenarioRooms[eduLowerScreen].setText("Extended Features", "Device is being used within the classroom as a way to develop better understandings on certain topics discussed in class and as a way to stimulate creativity. The kids are enjoying it, but they express that the headset itself is a little bit restrictive and want the head set to be more free moving.")
-  scenarioRooms[eduLowerAddScreen].setText("Additional Extended Features", "Device is being used within the classroom as a way to develop better understandings on certain topics discussed in class and as a way to stimulate creativity. The kids are enjoying it, but they express that the headset itself is a little bit restrictive and want the head set to be more free moving.");
-  scenarioRooms[budgetScreen].setText("Budget Problems", "Sponsors are complaining that you’re not making enough money with this device. They suggest to move over to a paid model service that limits functionality and uses curated user ads.");
-  scenarioRooms[sellScreen].setText("Ending Three", "You sell to a randomly selected company. Now that the decisions are out of your control the ending is displayed as good or bad.");
+  scenarioRooms[eduLowerAddScreen].setText("Additional Extended Features", "Additionally primary schools are saying that the UI is a little bit hard for the younger kids to understand. They're asking for there to be a friendlier UI for the kids to interact with");
+  scenarioRooms[budgetScreen].setText("Budget Problems", "Sponsors are complaining that you’re not making enough money with the headset devices. They suggest to move over to a paid model service that limits functionality and uses curated user ads.");
+  scenarioRooms[sellScreen].setText("Ending Three", "You sell to a randomly selected company. Because there was no other features added to the base model, the unreliable company you chose uses it to perform illegal activities. When they eventually get caught they put all the fall on you.");
   scenarioRooms[adScreen].setText("Ending Four", "Target market no longer can afford your device, now it’s marketed as a tech toy by the upper elite class that stops being relevant by the time another new gadget is introduced.");
   scenarioRooms[keepScreen].setText("Keep the Current Model", "By leaving the current model as it is, the budget has become tighter than before. By not implementing a paid model you have to make budget cuts elsewhere.");
-  scenarioRooms[salaryScreen].setText("Ending Five", "Staff is starting to leave, unpaid interns are not motivated in putting countless hours into working for barely any benefits. Product ends up becoming buggy.");
-  scenarioRooms[productScreen].setText("Cut Production Costs", "You went with cutting production costs, as a result the device keeps breaking and is buggy. Malfunctioning hardware is stirring up concern in the mothers anonymous forum. In the next week a rumor starts circulating that you’re brainwashing students.");
-  scenarioRooms[rumorScreen].setText("Ending Six", "You make a statement about the rumors and take full responsibility for the drop in performance. You personally start putting hours to fix the technology. The newer version you produce becomes a bigger hit and everyone is happy.");
+  scenarioRooms[salaryScreen].setText("Ending Five", "Staff is starting to leave, unpaid interns are not motivated in putting countless hours into working for barely any benefits. As a result the quality of the code drops and version deployments are constantly delayed with bugs.");
+  scenarioRooms[productScreen].setText("Cut Production Costs", "You went with cutting production costs, as a result the device keeps breaking and becomes a potential hazard to wearers. Malfunctioning hardware is stirring up concern in the mothers anonymous forum especially. In the next week a rumor starts circulating that your company is intentionally causing harm to their children's brains. They demand you recall the product");
+  scenarioRooms[rumorScreen].setText("Ending Six", "You make a statement about the rumors and take full responsibility for the drop in performance. You personally start putting hours to fix the technology. You release the newer safer model under a different name and sell it to a respectable organization where it is a hit. You get no money compensation for the work and have to remain off the grid the rest of your life after your controversy.");
   scenarioRooms[ignoreScreen].setText("Ending Seven", "You ignore the rumors and don’t address them. The mother’s association sues your product for potentially harming their children. You lose the case and are bared from ever working in the industry again.");
-
+  scenarioRooms[sellAltScreen].setText("Ending Eight", "You sell the device to another company, but because of the attention you added for the educational features, the technology is able to be reused in a similar area of impact. They get an award and you are credited for the base model, earning a small sum in prize money.")
 }
 
 //-------------- SUBCLASSES / YOUR DRAW CODE CAN GO HERE ---------------//
@@ -471,4 +479,105 @@ class Callibration extends PNGRoom {
 
   }
 }
+
+class EndingRoom extends PNGRoom {
+  constructor() {
+    super();    // call super-class constructor to initialize variables in PNGRoom
+
+    this.titleText = "";
+    this.bodyText = "";
+  }
+
+  setText( titleText, bodyText ) {
+    this.titleText = titleText;
+    this.bodyText = bodyText;
+    this.offset = 200;
+    this.drawY = 360 - this.offset;
+    this.drawX = 52;
+  }
+
+  draw() {
+    // this calls PNGRoom.draw()
+    super.draw();
+    console.log(adventureManager.getCurrentStateNum());
+    push();
+
+    fill(248, 248, 248, 180)
+    stroke(146, 161, 161);
+    rect(this.drawX - 15, this.drawY - 60, width - ((this.drawX - 15) * 2),height - (this.drawY +100), 30)
+
+    // title text
+    fill("#8D8D8D");
+    textAlign(LEFT);
+    textFont(headlineFont);
+    textSize(36);
+    noStroke();
+
+
+    text("$ " + budget, this.drawX + 200 , 60);
+
+    fill("#484848");
+    text("Budget: ",this.drawX , 60)
+    // title text
+    textSize(30);
+
+    text("- " + this.titleText, this.drawX , this.drawY);
+
+    textSize(30)
+    textFont(endFont);
+    fill("#5c5c5c");
+
+    text("Overall Budget Score: " + checkScore(), this.drawX, height/2 + 5);
+    text("Ending Score: " + allEndings[adventureManager.getCurrentStateNum()], this.drawX, height/2 + 55);
+    fill("#484848");
+
+    textSize(45)
+    text("Total Score: " + overallScore(), this.drawX, height/2 + 145);
+
+    textFont(bodyFont);
+    textSize(24);
+    fill("#8D8D8D");
+
+    text(this.bodyText, this.drawX , this.drawY + 60, width - (this.drawX*2),height - (this.drawY+100) );
+    pop();
+
+    clickables[3].visible = true;
+  }
+}
+
+function checkScore(){
+  if(budget >= 50000){
+    return "A"
+  } else if (budget >= 25000) {
+    return "B"
+  } else {
+    return "C"
+  }
+}
+
+function overallScore() {
+  var score1 = allEndings[adventureManager.getCurrentStateNum()];
+  var score2 = checkScore();
+  if(score1 === score2) {
+    return checkScore();
+  } else if ( score1 === "C" || score2 === "C") {
+    return "B-";
+  } else if ( score1 === "B" || score2 ==="B") {
+    return "B";
+  } else {
+    return "A";
+  }
+}
+
+function setupEndings(){
+  allEndings[endOneScreen] = "C";
+  allEndings[endTwoScreen] = "A";
+  allEndings[13] = "C";
+  allEndings[14] = "C";
+  allEndings[16] = "B";
+  allEndings[18] = "A";
+  allEndings[19] = "C";
+  allEndings[20] = "A";
+}
+
 
